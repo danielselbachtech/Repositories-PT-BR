e9e3e1251ae7bb3304b4b60f8c564e2d0281dd9e
danielselbachtech <danielselbach.fig@gmail.com>
2026-04-05 13:57:35 -0300

    atualizacao

--- 0.0-estudos/DIO/formacao-logica-de-programacao/desafios-de-projetos/desafio-01/src/desafio-01.js
+++ 0.0-estudos/DIO/formacao-logica-de-programacao/desafios-de-projetos/desafio-01/src/desafio-01.js
@@ -1,27 +1,31 @@
-let nome = "Esdras";
-let xpLista = [500, 1500, 3000, 6000, 7500, 8500, 9500, 11000];
+const NomeDoHeroi = "JackChan";
+let xpDoHeroi = 2000;
+let nivel;
 
-for (let i = 0; i < xpLista.length; i++) {
-    let xp = xpLista[i];
-    let nivel;
-
-    if (xp <= 1000) {
-        nivel = "Ferro";
-    } else if (xp <= 2000) {
-        nivel = "Bronze";
-    } else if (xp <= 5000) {
-        nivel = "Prata";
-    } else if (xp <= 7000) {
-        nivel = "Ouro";
-    } else if (xp <= 8000) {
-        nivel = "Platina";
-    } else if (xp <= 9000) {
-        nivel = "Ascendente";
-    } else if (xp <= 10000) {
-        nivel = "Imortal";
-    } else {
-        nivel = "Radiante";
+switch (true) {
+    case xpDoHeroi >= 0 && xpDoHeroi <= 1000:
+        nivel = 'Ferro';
+        break;
+    case xpDoHeroi >= 1001 && xpDoHeroi <= 2000:
+        nivel = 'Bronze';
+        break;
+    case xpDoHeroi >= 2001 && xpDoHeroi <= 5000:
+        nivel = 'Prata';
+        break;
+    case xpDoHeroi >= 5001 && xpDoHeroi <= 7000:
+        nivel = 'Ouro';
+        break;
+    case xpDoHeroi >= 7001 && xpDoHeroi <= 8000:
+        nivel = 'Platina';
+        break;
+    case xpDoHeroi >= 8001 && xpDoHeroi <= 9000:
+        nivel = 'Ascendente';
+        break;
+    case xpDoHeroi >= 9001 && xpDoHeroi <= 10000:
+        nivel = 'Radiante';
+        break;
+    default:
+        nivel = 'Desconhecido';
     }
 
-    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
-}
+console.log(`O Herói de nome ${NomeDoHeroi} está no nivel de ${nivel}!`);
